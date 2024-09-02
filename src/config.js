6d268aaf79468f81b03cdf1f32190dc8d7ef9e48
Delwar Hossain <77322550+DelwarHossain10@users.noreply.github.com>
2024-09-03 01:26:00 +0600

    message

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const imgBase = "https://apps.mis.digital/api/pharmacy-app/storage/";
-// export const apiBase = "https://api.acigroup.info/crm/api";
-export const apiBase = "http://127.0.0.1:8000/api";
+export const imgBase = "https://api.acigroup.info/crm/storage/";
+export const apiBase = "https://api.acigroup.info/crm/api";
+//export const apiBase = "http://127.0.0.1:8000/api";
 
