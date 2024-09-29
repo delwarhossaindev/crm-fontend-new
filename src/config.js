fc23196d0a3e983c8dc248d90801a2f844f0a080
Delwar Hossain <77322550+DelwarHossain10@users.noreply.github.com>
2024-09-29 17:56:05 +0600

    today

--- src/config.js
+++ src/config.js
@@ -4,6 +4,10 @@
 // export const imgBase = "https://crmapi.softifly.com/storage/";
 // export const apiBase = "https://crmapi.softifly.com/api";
 
-export const imgBase = "http://127.0.0.1:8000/storage/";
-export const apiBase = "http://127.0.0.1:8000/api";
+// export const imgBase = "http://127.0.0.1:8000/storage/";
+// export const apiBase = "http://127.0.0.1:8000/api";
+
+export const imgBase = "http://crmapi.delwarhossaindev.com/storage/";
+export const apiBase = "https://crmapi.delwarhossaindev.com/api";
+
 
